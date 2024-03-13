8613d31d15d63c0c64d984e7a604742201e3e195
Otto Hedger <a1891747@student.adelaide.edu.au>
2024-03-13 15:03:07 +1030

    Workshop 3

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "workshop.h"
+#include <iostream>
+
+int main(){
+    double ten = 10.2;
+    double array[] = {3,4,56,73,2};
+    int arraySize = sizeof(array) / sizeof(array[0]);
+    char star = 's';
+    double* ptrdoub = &ten;
+    char* str = &star;
+    int N = 5;
+    double *maxPtr = dynamicArray(N);
+    //changeValue(ptrdoub);
+    //printArray(array, arraySize) ;
+    //std::cout << arrayMax(array, arraySize) << std::endl;
+    std::cout << maxPtr << std::endl;
+    delete[] maxPtr;
+
+    return 0;
+}
