a640e15f56fd453b77f3a836e591ecd8187d7cf7
Otto Hedger <a1891747@student.adelaide.edu.au>
2024-03-14 11:03:37 +1030

    I read question 2 wrong

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
 void modifyArray(double* array, int size, double value){
-    double *arr2 = new double[size];
+
     for(int i=0; i<size; ++i){
-        arr2[i] = array[i] + value;
+        array[i] = array[i] + value;
     }
 
     //for(int i = 0;i < size; ++i){
-    //    std::cout << arr2[i] << std::endl;
+    //    std::cout << array[i] << std::endl;
     //}
 }
