ed4bf9de47f89beab9de6e07aa3132fb4cf7f36d
Otto Hedger <a1891747@student.adelaide.edu.au>
2024-03-14 11:00:57 +1030

    Fixing question 2 again?

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -3,11 +3,9 @@
 void modifyArray(double* array, int size, double value){
     double *arr2 = new double[size];
     for(int i=0; i<size; ++i){
-        arr2[i] = array[i];
-    };
-    for(int i = 0;i < size; ++i){
-        arr2[i] = arr2[i] + value;
+        arr2[i] = array[i] + value;
     }
+
     //for(int i = 0;i < size; ++i){
     //    std::cout << arr2[i] << std::endl;
     //}
--- main-1-2.cpp
+++ main-1-2.cpp
@@ -2,7 +2,7 @@
 extern void modifyArray(double* array, int size, double value);
 
 int main(){
-    double array[] = {4,2,3,1};
+    double array[] = {4.3,24.2,33.7,-24};
     int size =  sizeof(array)/sizeof(array[0]);
     double value = 23;
     modifyArray(array, size, value);
