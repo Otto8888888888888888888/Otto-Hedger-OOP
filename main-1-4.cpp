885413ca38be515eda2ea1526f5edbdf406f869b
Otto Hedger <a1891747@student.adelaide.edu.au>
2024-03-21 11:47:35 +1030

    completed prac

--- main-1-4.cpp
+++ main-1-4.cpp
@@ -5,6 +5,5 @@ int main(){
     int n=5;
     PersonList pl = createPersonList(n);
     PersonList list2 = shallowCopyPersonList(pl);
-    cout << "Person: " << list2.people[1].name << " Age: " << list2.people[1].age << endl;
     return 0;
 }
