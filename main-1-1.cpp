702e05431b62c116c67593c6e2370a64fc2f184b
Otto Hedger <a1891747@student.adelaide.edu.au>
2024-03-28 11:11:49 +1030

    Fixed code

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -6,8 +6,6 @@
 int main(){
     Unit unit1;
     unit1 = Unit(100, 2, 3.4);
-    std::cout << Unit::get_Num_Bedrooms(unit1) << std::endl;
-    std::cout << Unit::get_Value(unit1) << std::endl;
-    std::cout << Unit::get_Area(unit1) << std::endl;
+
     return 0;
 }
