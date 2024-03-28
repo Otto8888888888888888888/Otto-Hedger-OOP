95480ff6d6a1b61f3f6abed24b4631c2d3329e56
Otto Hedger <a1891747@student.adelaide.edu.au>
2024-03-28 11:57:14 +1030

    man i dont know how to code for part 2

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -5,17 +5,17 @@
 
 int main() {
     ApartmentBuilding ApartmentBuilding(5);
-    Unit unit1;
-    unit1 = Unit(100, 2, 3.4);
-    
-    Unit unit2;
-    unit2 = Unit(10, 20, 32);
-    Unit unit3;
-    unit3 = Unit(11, 23, 34);
-    Unit unit4;
-    unit4 = Unit(15, 7, 4);
-    Unit unit5;
-    unit5 = Unit(9, 14, 24);
+    // Unit unit1;
+    // unit1 = Unit(100, 2, 3.4);
+
+    // Unit unit2;
+    // unit2 = Unit(10, 20, 32);
+    // Unit unit3;
+    // unit3 = Unit(11, 23, 34);
+    // Unit unit4;
+    // unit4 = Unit(15, 7, 4);
+    // Unit unit5;
+    // unit5 = Unit(9, 14, 24);
     
 }
 
