ba8ff299ee1ba4a9785825f4be5fd202a5b896c6
Otto Hedger <a1891747@student.adelaide.edu.au>
2024-03-28 11:22:26 +1030

    Final code for part 1

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -6,6 +6,8 @@
 int main(){
     Unit unit1;
     unit1 = Unit(100, 2, 3.4);
-
+    // std::cout << unit1.get_Num_Bedrooms() << std::endl;
+    // std::cout << unit1.get_Value() << std::endl;
+    // std::cout << unit1.get_Area() << std::endl;
     return 0;
 }
