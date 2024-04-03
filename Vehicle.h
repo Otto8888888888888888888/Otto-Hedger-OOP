e4cb4211cb0713d95c84550fd33f3c5b5a4dd610
Otto Hedger <a1891747@student.adelaide.edu.au>
2024-04-03 11:02:20 +1030

    Yeah yeah

---
+++ Vehicle.h
@@ -0,0 +1,15 @@
+using namespace std;
+
+class Vehicle{
+    private:
+    time_t timeOfEntry;
+    int ID;
+    public:
+
+
+};
+Vehicle::Vehicle(time_t s_timeOfEntry, int s_ID)
+{
+    timeOfEntry = s_timeOfEntry;
+    ID = s_ID
+}
