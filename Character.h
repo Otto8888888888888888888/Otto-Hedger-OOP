c0af9c1d71d59281426ec348ed969ea0d3fc98c8
Otto Hedger <a1891747@student.adelaide.edu.au>
2024-05-30 11:50:23 +0930

    please

--- Character.h
+++ Character.h
@@ -10,7 +10,9 @@ class Character: public Cell{
 };
 
 void Character::move(int dx, int dy){
-    setPos(dx, dy);
+    int newX = std::get<0>(position) + dx;
+    int newY = std::get<1>(position) + dy;
+    setPos(newX, newY);
 }
 
 #endif
