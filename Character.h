c57a46c0b7bdb8c7641e9a345a605b6ba4567b3d
Otto Hedger <a1891747@student.adelaide.edu.au>
2024-05-30 11:33:47 +0930

    AHHHH

--- Character.h
+++ Character.h
@@ -1,5 +1,21 @@
 #ifndef CHARACTER_H
 #define CHARACTER_H
+#include "Cell.h"
 
+class Character: public: Cell{
+    private:
+    public:
+    Character(int x, int y);
+    void move(int dx, int dy)
+};
+
+Character::Character(int x, int y){
+    position = std::make_tuple(x,y);
+    type = 'C';
+}
+
+Character::move(int dx, int dy){
+    position = setPos(dx, dy);
+}
 
 #endif
--- Trap.h
+++ Trap.h
@@ -1,7 +1,31 @@
 #ifndef TRAP_H
 #define TRAP_H
+#include "Effect.h"
+#include "Cell.h"
 
-class Trap: public: Cell,Effect{
-
+class Trap: public: Cell public: Effect{
+    private:
+    bool active;
+    public:
+    Trap(int x, int y);
+    bool isActive();
+    void apply(Cell& cell);
 };
+
+Trap::Trap(int x, int y){
+    position = std::make_tuple(x,y);
+    type = 'T';
+    active = true;
+}
+
+Bool Trap::isActive(){
+    return active;
+}
+
+void Trap::apply(Cell& cell){
+    if active = true{
+        cell.setType('T');
+        active = false;
+    }
+}
 #endif
